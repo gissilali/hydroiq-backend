fdb94afdf6463dba6e859c65ddb0549e8b2586e1
silali <gibsonsilali@gmail.com>
2021-01-18 00:57:43 +0300

    added database credentials

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-3-215-76-208.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd96bb4pp2ifnab'),
+            'username' => env('DB_USERNAME', 'cukkdaklrwzzux'),
+            'password' => env('DB_PASSWORD', '939d86004464cb4c9917df92404624a3361485b4c38fd3395fa7d01734a05891'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
